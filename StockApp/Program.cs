173a849cd9bfc891fc48598c656c5c3a4ca524ae
marwannelsayed <marwanyehiaelsayed@gmail.com>
2024-07-16 10:10:09 +0300

    last

---
+++ StockApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+var hubConnection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5191/StockHub")
+    .Build();
+
+hubConnection.On<Stock>("ReceiveStockUpdate", (stock) =>
+{
+    Console.WriteLine($"Received stock update: {stock.StockSymbol} - {stock.Price}");
+});
+
+await hubConnection.StartAsync();
+
+Console.WriteLine("Connected to SignalR server. Press any key to exit...");
+Console.ReadKey();
+
+await hubConnection.DisposeAsync();
+
+public class Stock
+{
+    public string StockSymbol { get; set; }
+    public decimal Price { get; set; }
+}
